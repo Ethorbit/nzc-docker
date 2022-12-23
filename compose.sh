099d6f3512cccb0b3064595b3f23b35ff69af563
Ethorbit <Ethorbit@protonmail.com>
2022-12-22 22:05:03 -0800

    Move data_dir directory creation instructions

--- compose.sh
+++ compose.sh
@@ -1,11 +1,14 @@
 #!/bin/bash 
-[ -z "$DATA_DIR" ] && export DATA_DIR="./data"
 source ".env"
+
+[ -z "$DATA_DIR" ] && export DATA_DIR="./data"
+[ ! -d "$DATA_DIR" ] && mkdir -p "$DATA_DIR/${gmod,discord}"
 mkdir "$DATA_DIR" 2> /dev/null
+
 export DISK=$(df "$DATA_DIR" | tail -1 | cut -d " " -f 1)
 
 gmod()
-{   
+{ 
     PORT=27014 # Starts at this + 1
     for i in $(seq $GMOD_COUNT); do
         SERVER_NUMBER="$i" GMOD_PORT=$(($PORT + 1)) CPU=$(($i - 1)) docker-compose -f "gmod-server.yml" $@ &
@@ -19,7 +22,6 @@ svencoop()
 
 discord()
 {
-    [ ! -d "$DATA_DIR" ] && mkdir -p "$DATA_DIR/${discord}"
     docker-compose -f "discord-bots.yml" $@ || exit 1 &
 }
 
