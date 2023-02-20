71df0642bb0197e60e2b6e55e64c039d3384840e
Ethorbit <Ethorbit@protonmail.com>
2023-02-20 05:38:17 -0800

    More progress towards users container

--- compose/build/users_and_groups/start.sh
+++ compose/build/users_and_groups/start.sh
@@ -7,9 +7,9 @@ done
 chmod 644 /mnt/passwd /mnt/group /mnt/env
 chmod 640 /mnt/shadow
 
-# Parse settings to add the desired users and groups
+# Create the users and groups specified by settings.yml
 if [[ ! -f "/mnt/settings.yml" ]]; then 
-    echo "Configuration /mnt/settings.yml is missing!"
+    echo "/mnt/settings.yml is missing!"
     exit 1
 fi  
 
@@ -17,28 +17,58 @@ settings=$(cat /mnt/settings.yml | envsubst)
 groups=$(echo "$settings" | yq ".groups | keys | .[]")
 users=$(echo "$settings" | yq ".users | keys | .[]")
 
-for group in "$groups"; do
-    #echo "$group"
-    id=$(echo "$groups" | yq ".$group.id")
-    echo "Test id $id"
+echo "Creating groups.."
+for group in $groups; do
+    IFS=, read id system \
+        < <(echo "$settings" | yq -r ".groups.$group | [ .id, .system ] | @csv" \
+        | sed "s/null\|false//g")
+
+    addgroup "$group" \
+        `[[ "$id" =~ [0-9]+ ]] && echo -g "$id"` \
+        `[[ ! -z "$system" ]] && echo -S`
+
+    [[ $? -eq 0 ]] && echo "$group"
 done
 
-for user in "$users"; do    
-    #echo "$user"
-    groups=$(echo "$users" | yq ".$user.groups | keys | .[]")
-    #echo "$groups"
+echo "Creating users.."
+for user in $users; do 
+    IFS=, read home id password shell system groups \
+        < <(echo "$settings" | \
+        yq -r ".users.$user | [.home, .id, .password, .shell, .system, .groups.[]] | @csv" \
+        | sed "s/null//g")
+    
+    adduser "$user" -D \
+        `[[ "$home" = "false" ]] && echo -H` \
+        `[[ ! -z "$home" && "$home" != "false" ]] && echo -h "$home"` \
+        `[[ "$id" =~ [0-9]+ ]] && echo -G "$user" -u "$id"` \
+        `[[ ! -z "$shell" ]] && echo -s "$shell"` \
+        `[[ ! -z "$system" ]] && echo -S`
+ 
+    if [[ $? -eq 0 ]]; then
+        echo "$user"
+    
+        if [[ ! -z "$password" && "$password" != "false" ]]; then
+            echo "$user:$password" | chpasswd
+        fi
+   
+        IFS=,
+        for group in $groups; do 
+            addgroup "$user" "$group"
+        done
+    fi
 done
 
-# Now that we've added all the users and groups, we will 
-# output our user files into /mnt, which other 
-# containers can have mounted to them as READ-ONLY.
+# Output our user files to the /mnt volume, which 
+# other containers can then have mounted as read-only
+# to have the same users and groups.
 for f in passwd group shadow; do
-    getent "$f" > "/mnt/$f"
+    cat "/etc/$f" > "/mnt/$f"
 done
 
-# We will export the users and their ids to an env file which can
+# Export the uid and gids to an env file which can
 # be used in a docker-compose --env-file to make it possible
-# to use user and group names created by the container instead of ids
+# to use this container's user and group names instead of
+# the ids or the names from the host
 getent passwd | while IFS=: read -r user _ uid gid _; do 
-    printf '%s_u=%i\n%s_g=%i\n' "$user" "$uid" "$user" "$gid" >> /mnt/env 2>&1 >&1
+    printf '%s_u=%i\n%s_g=%i\n' "$user" "$uid" "$user" "$gid" >> /mnt/env
 done
