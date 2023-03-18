85da9bb5bb480f902808052fee82a94a839afcc0
Ethorbit <Ethorbit@protonmail.com>
2023-03-17 22:48:02 -0700

    Improve permissions, add ACLs

--- compose/build/gmod-server/container-start.sh
+++ compose/build/gmod-server/container-start.sh
@@ -11,11 +11,11 @@ done < "${SHARED_CACHE}"
 
 # Merge shared and server using only symlinks
 # Save each added symlink to the cache
-cp --verbose -ans "${SHARED_DIR}/"* "${SERVER_DIR}/" | awk '{ print $NF }' >> "${SHARED_CACHE}"
+cp --verbose --preserve=timestamp -dRns "${SHARED_DIR}/"* "${SERVER_DIR}/" | \
+    awk '{ print $NF }' >> "${SHARED_CACHE}"
 
-echo "Setting file permissions.."
-chmod 2770 -fR "${SERVER_DIR}/"
+#echo "Setting file permissions.."
+#chmod 2770 -fR "${SERVER_DIR}/"
 
-# Now start the steamcmd-server start.sh
-# this will also start up the server
+# Now start the steamcmd-server's start.sh
 exec /start.sh
