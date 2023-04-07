e239ee351b7c665690824a63b6fd609980944dbb
Ethorbit <Ethorbit@protonmail.com>
2023-04-07 15:39:58 -0700

    Undo recursive permissions in svencoop start

--- compose/build/svencoop-server/server-start.sh
+++ compose/build/svencoop-server/server-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-chmod 2770 -R "$SERVER_DIR/"
+#chmod 2770 -R "$SERVER_DIR/"
 
 "${SERVER_DIR}/svends_run" -autoupdate -steam_dir "${STEAMCMD_DIR}" -steamcmd_script "${SERVER_DIR}/${STEAMCMD_UPDATE_SCRIPT}" "${START_ARGS}"
