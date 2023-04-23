c58f04d0a4cf2f37c1230158bc5a72fb3b0d33c3
Ethorbit <Ethorbit@protonmail.com>
2023-04-22 20:28:58 -0700

    Add webserver network to opengamepanel

--- compose/build/opengamepanel/entrypoint.sh
+++ compose/build/opengamepanel/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 umask 007
 
-if [ -z $(ls -A /mnt) ]; then
+if [ -z $(ls -A /panel/) ]; then
     cp -rap "$HOME"/* /panel/
+    curl -k "https://${WEB_PAGE}/install.php?step=2"
 fi
 
 exec "$@"
