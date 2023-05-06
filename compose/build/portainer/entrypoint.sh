573053aaea3a020c7603c4ec1f48c61444d95f3b
Ethorbit <Ethorbit@protonmail.com>
2023-05-05 20:38:28 -0700

    Login to Portainer admin within script

--- compose/build/portainer/entrypoint.sh
+++ compose/build/portainer/entrypoint.sh
@@ -4,6 +4,7 @@ chmod -R 760 /data/
 chmod 755 /data
 
 c_loc=/data/.install_cookie
+jwt_token=
 
 setup_users_and_roles()
 {
@@ -16,12 +17,23 @@ login_to_admin()
 {
     echo "Logging into admin.."
     response=$(curl -ksLf -X POST -H "Content-Type: application/json" \
-        -w "%{http_code}\n" -c "$c_loc" -b "$c_loc" \
+        -c "$c_loc" -b "$c_loc" \
         -d "{ \"Username\":\"admin\", \"Password\":\"${PORTAINER_ADMIN_PASSWORD}\" }" \
         "${WEB_PAGE}/api/auth")
     
-    [ "$response" -ne 200 ] && echo "Failed to login to admin ($?) - $response" >&2 && sleep 1 && login_to_admin && return
-    echo "Response: $response"
+    curl_err="$?"
+    
+    echo "$response" | jq -e .jwt 2> /dev/null > /dev/null
+    jq_err="$?"
+   
+    # Check if failed or invalid JSON.
+    if [ "$curl_err" -ne 0 ] || [ "$jq_err" -ne 0 ]; then
+        echo "Failed to login to admin (curl - $curl_err, jq - $jq_err)" >&2 && sleep 1 && login_to_admin
+    else
+        jwt_token=$(echo "$response" | jq -r .jwt)
+        jwt_token_snipped=$(echo "$jwt_token" | head -c 10)
+        echo "Logged in, auth token retrieved - ${jwt_token_snipped}<snipped>"
+    fi
 }
 
 created_admin=
@@ -35,8 +47,8 @@ setup_admin()
         -d "{ \"Username\":\"admin\", \"Password\":\"${PORTAINER_ADMIN_PASSWORD}\" }" \
         "${WEB_PAGE}/api/users/admin/init")
     
-    [ "$response" -ne 200 ] && echo "Failed to set the admin password ($?) - $response" >&2 && sleep 1 && setup_admin && return
-    created_admin=1
+    [ "$response" -ne 200 ] && echo "Failed to set the admin password ($?) - $response" >&2 &&\
+        sleep 1 && setup_admin && return || created_admin=1
 }
 
 start_install()
