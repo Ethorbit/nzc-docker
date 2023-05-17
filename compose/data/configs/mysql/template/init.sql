1a5a1beb2d7d07776bc0759cf8b796718c4b63e2
Ethorbit <Ethorbit@protonmail.com>
2023-05-17 03:51:05 -0700

    Make improvements to mysql and phpmyadmin

--- compose/data/configs/mysql/template/init.sql
+++ compose/data/configs/mysql/template/init.sql
@@ -1,24 +1,23 @@
 CREATE USER 'admin'@'%' IDENTIFIED BY '${ADMIN_PASSWORD}';
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
 
-CREATE ROLE 'gmod';
-CREATE ROLE 'svencoop';
+CREATE ROLE gmod;
+CREATE ROLE svencoop;
 
 CREATE DATABASE gmod_nzombies;
 CREATE DATABASE gmod_anticheat;
 CREATE DATABASE svencoop;
-GRANT ALL PRIVILEGES ON gmod_nzombies.* TO 'gmod';
-GRANT ALL PRIVILEGES ON gmod_anticheat.* TO 'gmod';
-GRANT ALL PRIVILEGES ON svencoop.* TO 'svencoop';
+
+GRANT INSERT, UPDATE, DELETE ON `gmod%`.* TO gmod;
+GRANT INSERT, UPDATE, DELETE ON `svencoop%`.* TO svencoop;
 
 # Your custom users AKA staff members
+CREATE USER 'gmod_lua'@'gmod' IDENTIFIED BY '${GMOD_LUA_PASSWORD}';
 CREATE USER 'doormatt'@'%' IDENTIFIED BY '${DOORMATT_PASSWORD}';
 CREATE USER 'blunto'@'%' IDENTIFIED BY '${BLUNTO_PASSWORD}';
 CREATE USER 'berb'@'%' IDENTIFIED BY '${BERB_PASSWORD}';
 CREATE USER 'freeman'@'%' IDENTIFIED BY '${FREEMAN_PASSWORD}';
 CREATE USER 'pepe'@'%' IDENTIFIED BY '${PEPE_PASSWORD}';
-GRANT 'gmod' TO 'doormatt'@'%';
-GRANT 'gmod' TO 'blunto'@'%';
-GRANT 'gmod' TO 'berb'@'%';
-GRANT 'svencoop' TO 'freeman'@'%';
-GRANT 'svencoop' TO 'pepe'@'%';
+
+GRANT gmod TO 'gmod_lua'@'gmod', 'doormatt'@'%', 'blunto'@'%', 'berb'@'%';
+GRANT svencoop TO 'freeman'@'%', 'pepe'@'%';
