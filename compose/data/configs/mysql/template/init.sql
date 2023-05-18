b9e00900c0bff8fde4034d3e3ee27bdec6e3e6d4
Ethorbit <Ethorbit@protonmail.com>
2023-05-18 00:29:00 -0700

    Make MySQL and PHPMyAdmin improvements

--- compose/data/configs/mysql/template/init.sql
+++ compose/data/configs/mysql/template/init.sql
@@ -1,23 +1,29 @@
 CREATE USER 'admin'@'%' IDENTIFIED BY '${ADMIN_PASSWORD}';
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
 
-CREATE ROLE gmod;
-CREATE ROLE svencoop;
-
 CREATE DATABASE gmod_nzombies;
 CREATE DATABASE gmod_anticheat;
 CREATE DATABASE svencoop;
 
-GRANT INSERT, UPDATE, DELETE ON `gmod%`.* TO gmod;
-GRANT INSERT, UPDATE, DELETE ON `svencoop%`.* TO svencoop;
+CREATE ROLE gmod;
+CREATE ROLE svencoop;
+GRANT ALL ON `gmod%`.* TO gmod;
+GRANT ALL ON `svencoop%`.* TO svencoop;
 
-# Your custom users AKA staff members
-CREATE USER 'gmod_lua'@'gmod' IDENTIFIED BY '${GMOD_LUA_PASSWORD}';
+/* Internal users */
+CREATE USER 'gmod'@'gmod' IDENTIFIED BY '${GMOD_PASSWORD}';
+CREATE USER 'svencoop'@'svencoop' IDENTIFIED BY '${SVENCOOP_PASSWORD}';
+GRANT gmod TO 'gmod'@'gmod';
+GRANT svencoop TO 'svencoop'@'svencoop';
+
+/* Your custom users AKA staff members */
 CREATE USER 'doormatt'@'%' IDENTIFIED BY '${DOORMATT_PASSWORD}';
 CREATE USER 'blunto'@'%' IDENTIFIED BY '${BLUNTO_PASSWORD}';
 CREATE USER 'berb'@'%' IDENTIFIED BY '${BERB_PASSWORD}';
 CREATE USER 'freeman'@'%' IDENTIFIED BY '${FREEMAN_PASSWORD}';
 CREATE USER 'pepe'@'%' IDENTIFIED BY '${PEPE_PASSWORD}';
-
-GRANT gmod TO 'gmod_lua'@'gmod', 'doormatt'@'%', 'blunto'@'%', 'berb'@'%';
-GRANT svencoop TO 'freeman'@'%', 'pepe'@'%';
+GRANT gmod TO 'doormatt'@'%';
+GRANT gmod TO 'blunto'@'%';
+GRANT gmod TO 'berb'@'%';
+GRANT svencoop TO 'freeman'@'%';
+GRANT svencoop TO 'pepe'@'%';
--- compose/data/configs/phpmyadmin/template/config.inc.php
+++ compose/data/configs/phpmyadmin/template/config.inc.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
  * This is needed for cookie based authentication to encrypt the cookie.
  * Needs to be a 32-bytes long string of random bytes. See FAQ 2.10.
  */
-$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = '${PHPMYADMIN_BLOWFISH_SECRET}'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
@@ -24,6 +24,8 @@ $i = 0;
  * First server
  */
 $i++;
+$cfg['Servers'][$i]['socket'] = '';
+
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
