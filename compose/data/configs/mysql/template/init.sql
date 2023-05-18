8e7f5994746bc1bc1eb3edeb335de7a72b2e24ad
Ethorbit <Ethorbit@protonmail.com>
2023-05-18 03:23:17 -0700

    Fix MySQL user permissions

--- compose/data/configs/mysql/template/init.sql
+++ compose/data/configs/mysql/template/init.sql
@@ -22,8 +22,8 @@ CREATE USER 'blunto'@'%' IDENTIFIED BY '${BLUNTO_PASSWORD}';
 CREATE USER 'berb'@'%' IDENTIFIED BY '${BERB_PASSWORD}';
 CREATE USER 'freeman'@'%' IDENTIFIED BY '${FREEMAN_PASSWORD}';
 CREATE USER 'pepe'@'%' IDENTIFIED BY '${PEPE_PASSWORD}';
-GRANT gmod TO 'doormatt'@'%';
-GRANT gmod TO 'blunto'@'%';
-GRANT gmod TO 'berb'@'%';
-GRANT svencoop TO 'freeman'@'%';
-GRANT svencoop TO 'pepe'@'%';
+
+GRANT gmod TO 'doormatt'@'%', 'blunto'@'%', 'berb'@'%';
+GRANT svencoop TO 'freeman'@'%', 'pepe'@'%';
+SET DEFAULT ROLE gmod TO 'doormatt', 'blunto', 'berb';
+SET DEFAULT ROLE svencoop TO 'freeman', 'pepe';
