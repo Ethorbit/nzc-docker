a17b4d15f2e28ba52eb3b6f85ecd6efca6864e51
Ethorbit <Ethorbit@protonmail.com>
2023-05-20 01:44:42 -0700

    Fix game server RCON_PASSWORD

--- compose/build/gmod-server/server-start.sh
+++ compose/build/gmod-server/server-start.sh
@@ -3,4 +3,4 @@
 # Override the default start_args for nZombies
 #START_ARGS="-port ${PORT} -tickrate 33 -disableluarefresh +maxplayers 15 +gamemode nzombies +map nz_kino_der_toten"
 
-"${SERVER_DIR}/srcds_run" -autoupdate -steam_dir "${STEAMCMD_DIR}" -steamcmd_script "${SERVER_DIR}/${STEAMCMD_UPDATE_SCRIPT_NOVALIDATE}" "${START_ARGS}"
+"${SERVER_DIR}/srcds_run" -autoupdate -steam_dir "${STEAMCMD_DIR}" -steamcmd_script "${SERVER_DIR}/${STEAMCMD_UPDATE_SCRIPT_NOVALIDATE}" "${START_ARGS}" +rcon_password "${RCON_PASSWORD}"
