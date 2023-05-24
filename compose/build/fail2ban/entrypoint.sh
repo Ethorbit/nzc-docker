3e06e1e464a796e74d54b2ea332c02a6956c02da
Ethorbit <Ethorbit@protonmail.com>
2023-05-23 18:30:46 -0700

    Fix rsyslog and Fail2ban support

--- compose/build/fail2ban/entrypoint.sh
+++ compose/build/fail2ban/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-#if [ -z $(ls -A /etc/fail2ban/) ]; then
-#cp -rap /etc/fail2ban_original/* /etc/fail2ban/
-#chown 770 -R /etc/fail2ban/
-#fi
+
+# Fail2ban will just crash if any of
+# the specified log files don't exist.
+# We're just going to create every
+# non-existent file specified (no dirs included)
+# (AKA what Fail2ban SHOULD do)
+for path in $(grep -rhEo '(\/[\.|a-zA-Z0-9_-]+)+' /etc/fail2ban/jail.d/); do 
+    [ ! -f "$path" ] && touch "$path" 2> /dev/null
+done
 
 exec /usr/bin/fail2ban-server "$@"
