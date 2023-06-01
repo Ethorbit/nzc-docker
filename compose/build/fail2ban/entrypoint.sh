113bc09f7cf3452a5d44d3efd8505b186261421f
Ethorbit <Ethorbit@protonmail.com>
2023-05-31 22:53:27 -0700

    Fix and improve fail2ban auto log creation

--- compose/build/fail2ban/entrypoint.sh
+++ compose/build/fail2ban/entrypoint.sh
@@ -5,8 +5,13 @@
 # We're just going to create every
 # non-existent file specified (no dirs included)
 # (AKA what Fail2ban SHOULD do)
-for path in $(grep -rhEo '(\/[\.|a-zA-Z0-9_-]+)+' /etc/fail2ban/jail.d/); do 
-    [ ! -f "$path" ] && touch "$path" 2> /dev/null
+for jail_file in /etc/fail2ban/jail.d/*; do
+    [ ! -f "$jail_file" ] && continue
+
+    for log_file in $(cat "$jail_file" | grep 'logpath' | cut -d '=' -f 2 | cut -d ' ' -f 2 | sed 's|^[\s]*||'); do
+        [ ! -f "$log_file" ] && touch "$log_file" 2> /dev/null &&\
+            echo "Created $log_file because it didn't exist."
+    done
 done
 
 exec /usr/bin/fail2ban-server "$@"
