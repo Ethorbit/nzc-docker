c551ee34676188d6cd9e0f506cad48e37bb4c04a
Ethorbit <Ethorbit@protonmail.com>
2023-06-06 02:05:55 -0700

    Suppress output for ssl healthcheck

--- compose/build/mkcert/healthcheck.sh
+++ compose/build/mkcert/healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 [ -f "$PUBLIC_KEY" ] && [ -s "$PUBLIC_KEY" ] &&\
     [ -f "$PRIVATE_KEY" ] && [ -s "$PRIVATE_KEY" ] &&\
-    openssl x509 -noout -pubkey -in "$PUBLIC_KEY" &&\
-    openssl pkey -pubout -noout -in "$PRIVATE_KEY" ||\
+    openssl x509 -noout -pubkey -in "$PUBLIC_KEY" 2> /dev/null > /dev/null &&\
+    openssl pkey -pubout -noout -in "$PRIVATE_KEY" 2> /dev/null > /dev/null ||\
     exit 1
