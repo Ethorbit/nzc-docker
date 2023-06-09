72b1fc29d24df8d383a4402c7ae790c18852d80b
Ethorbit <Ethorbit@protonmail.com>
2023-06-09 02:36:22 -0700

    Move certificate perms from Nginx to ssl_certificates.yml
    Since both mkcert and certbot run 24/7 and because Certbot receives files over the network, it probably makes sense for them to run unprivileged. Mkcert still runs as root because it's local and needs to install a certificate to the host for their browser.

--- compose/build/mkcert/entrypoint.sh
+++ compose/build/mkcert/entrypoint.sh
@@ -3,5 +3,10 @@
     mkcert -install &&\
     mkcert -cert-file /mnt/mkcert.pem -key-file /mnt/mkcert.key \
     ${PRIVATE_DOMAIN_NAME} *.${PRIVATE_DOMAIN_NAME}
-    
+
+chown mkcert:mkcert "$PUBLIC_KEY"
+chown mkcert:mkcert "$PRIVATE_KEY"
+chmod 650 "$PUBLIC_KEY"
+chmod 650 "$PRIVATE_KEY"
+
 /healthcheck.sh 2> /dev/null > /dev/null && sleep inf || rm -f "$PUBLIC_KEY" && rm -f "$PRIVATE_KEY" && exit 1
