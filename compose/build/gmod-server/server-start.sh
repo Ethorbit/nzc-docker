73ff5d56026717eb948865d1567b9cd20a25d529
Ethorbit <Ethorbit@protonmail.com>
2023-07-18 16:46:45 -0700

    Fix gmod server

--- compose/build/gmod-server/server-start.sh
+++ compose/build/gmod-server/server-start.sh
@@ -3,4 +3,5 @@
 # Override the default start_args for nZombies
 #START_ARGS="-port ${PORT} -tickrate 33 -disableluarefresh +maxplayers 15 +gamemode nzombies +map nz_kino_der_toten"
 
-"${SERVER_DIR}/srcds_run" -autoupdate -steam_dir "${STEAMCMD_DIR}" -steamcmd_script "${SERVER_DIR}/${STEAMCMD_UPDATE_SCRIPT_NOVALIDATE}" "${START_ARGS}" +sv_setsteamaccount "${STEAM_LOGIN_TOKEN}" +rcon_password "${RCON_PASSWORD}"
+# Add -autoupdate if you care, but just know that it will take forever fetching updates on each startup, causing server startup to be delayed for no reason..
+"${SERVER_DIR}/srcds_run" -steam_dir "${STEAMCMD_DIR}" -steamcmd_script "${SERVER_DIR}/${STEAMCMD_UPDATE_SCRIPT_NOVALIDATE}" "${START_ARGS}" +sv_setsteamaccount "${STEAM_LOGIN_TOKEN}" +rcon_password "${RCON_PASSWORD}"
