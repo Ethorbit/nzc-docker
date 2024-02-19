ccf7c64a4758524b65f02420c0426a9dc4f238fc
Ethorbit <Ethorbit@protonmail.com>
2024-02-19 04:38:02 -0800

    Set -port for gmod start script
    Needed or else only the 27015 port server will be joinable

--- compose/build/gmod-server/server-start.sh
+++ compose/build/gmod-server/server-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Override the default start_args for nZombies
-#START_ARGS="-port ${PORT} -tickrate 33 -disableluarefresh +maxplayers 15 +gamemode nzombies +map nz_kino_der_toten"
+# Uncomment for nZombies
+#START_ARGS="-tickrate 33 -disableluarefresh +maxplayers 15 +gamemode nzombies +map nz_kino_der_toten"
 
 # Add -autoupdate if you care, but just know that it will take forever fetching updates on each startup, causing server startup to be delayed for no reason..
-"${SERVER_DIR}/srcds_run" -steam_dir "${STEAMCMD_DIR}" -steamcmd_script "${SERVER_DIR}/${STEAMCMD_UPDATE_SCRIPT_NOVALIDATE}" "${START_ARGS}" +sv_setsteamaccount "${STEAM_LOGIN_TOKEN}" +rcon_password "${RCON_PASSWORD}"
+"${SERVER_DIR}/srcds_run" -steam_dir "${STEAMCMD_DIR}" -steamcmd_script "${SERVER_DIR}/${STEAMCMD_UPDATE_SCRIPT_NOVALIDATE}" "-port ${PORT} ${START_ARGS}" +sv_setsteamaccount "${STEAM_LOGIN_TOKEN}" +rcon_password "${RCON_PASSWORD}"
