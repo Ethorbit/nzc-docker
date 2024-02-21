31a4039924b9a6cc1e8778cc0273e59395cafa5f
Ethorbit <Ethorbit@protonmail.com>
2024-02-21 00:19:56 -0800

    Fix Sven Co-op DLC script not running

--- compose/build/svencoop-server/server-start.sh
+++ compose/build/svencoop-server/server-start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #chmod 2770 -R "$SERVER_DIR/"
 
+"${SERVERS_DIR}/${PAID_SCRIPT}"
 "${SERVER_DIR}/svends_run" -port "${PORT}" -steam_dir "${STEAMCMD_DIR}" -steamcmd_script "${SERVERS_DIR}/${STEAMCMD_UPDATE_SCRIPT}" "${START_ARGS}" +sv_setsteamaccount "${STEAM_LOGIN_TOKEN}" +rcon_password "${RCON_PASSWORD}"
