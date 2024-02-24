9ba92c45ce44c3b505913df161550a407b8ee2c0
Ethorbit <Ethorbit@protonmail.com>
2024-02-24 04:23:38 -0800

    Add rsyslog wrapper script
    Created to add future logrotate support to rsyslog container without logrotate causing container to restart due to killing rsyslog process post-rotation

--- compose/build/rsyslog/entrypoint.sh
+++ compose/build/rsyslog/entrypoint.sh
@@ -1,4 +1,39 @@
 #!/bin/sh
-readonly PID_FILE="/var/run/rsyslogd.pid"
-rm "${PID_FILE}" 2> /dev/null
-/usr/sbin/rsyslogd -i "${PID_FILE}" "$@"
+readonly RSYSLOG_PID_FILE="/var/run/rsyslogd.pid"
+readonly RSYSLOG_ARGS="$@"
+
+start_rsyslog() {
+    echo "Starting rsyslog.."
+    pkill rsyslogd
+    rm "$RSYSLOG_PID_FILE" 2> /dev/null
+    /usr/sbin/rsyslogd -i "$RSYSLOG_PID_FILE" "$RSYSLOG_ARGS" 2> /dev/null > /dev/null
+}
+
+rsyslog_loop() {
+    echo "Starting rsyslog loop."
+
+    while :; do
+        [ -f "$RSYSLOG_PID_FILE" ] &&\
+            [ -d "/proc/$(cat $RSYSLOG_PID_FILE)" ] || start_rsyslog
+    
+        sleep 2
+    done
+
+    echo "rsyslog_loop stopped when it shouldn't have!" ; kill $$ ; exit 1
+}
+
+logrotate_loop() {
+    echo "Starting logrotate loop."
+    
+    while :; do
+        sleep 1h
+
+        # Add logrotate command(s) here.
+    done
+
+    echo "logrotate_loop stopped when it shouldn't have!" ; kill $$ ; exit 1
+}
+
+rsyslog_loop &
+logrotate_loop &
+sleep infinity
