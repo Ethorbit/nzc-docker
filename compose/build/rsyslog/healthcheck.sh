84c22b559ff5f5b0c671fbf1d51a6e26e7449a17
Ethorbit <Ethorbit@protonmail.com>
2024-03-22 17:14:47 -0700

    Add healthcheck for rsyslog and sshd

--- compose/build/rsyslog/healthcheck.sh
+++ compose/build/rsyslog/healthcheck.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-pidof rsyslogd > /dev/null || exit 1
-# below does not work and I don't know why
-#nc -z -t -w1 127.0.0.1 514 2> /dev/null || exit 1
+# unused, only needed if we go back to using multiple instances of rsyslog
